53ffa34b8c497128c972546d8cf7c3186fea8bd0
fredy <jhonbernal542@gmail.com>
2023-01-19 15:18:00 -0500

    finish header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <meta name="description" content="Aplicacion todo list" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
       
     </div>
   )
